1171cb58f20348f3abf94a69310e1586bc1bf488
Henry <hengro44@gmail.com>
2021-12-20 09:59:59 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+export const App = () => {
+  <div>
+
+    <Router>
+      
+      <Routes>
+        {/* Example Routes */}
+        {/* <Route path="/" element={<LoginPage />} /> */}
+        {/* <Route path="/home" element={<HomePage />} /> */}
+        {/* <Route path="/profile" element={<Profile />} /> */}
+      </Routes>
+                    
+    </Router>
+
+  </div> 
+}
